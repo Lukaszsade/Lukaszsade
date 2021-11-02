c088095991350037f2cfced28e888edbbeead35b
Lukasz Janusz <lukasz.t.janusz@gmail.com>
2021-11-02 10:36:04 +0100

    init ACCordion

--- KODILLA-projekty-1/project-pizzeria/src/js/script.js
+++ KODILLA-projekty-1/project-pizzeria/src/js/script.js
@@ -61,6 +61,8 @@
 
       thisProduct.renderInMenu();
       console.log('new Product: ', thisProduct);
+      thisProduct.initAccordion();
+
     }
 
     renderInMenu() {
@@ -78,6 +80,30 @@
 
       //* add element to menu 
       menuContainer.appendChild(thisProduct.element);
+
+    }
+
+    initAccordion() {
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log('accordionTrigger: ', this.accordionTrigger);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function (event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(
+          select.all.menuProductsActive
+        );
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct !== thisProduct.element && activeProduct !== null) {
+          activeProduct.classList.remove('active');
+        }
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle('active');
+      });
 
     }
   }
